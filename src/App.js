3288a7c1620449c02a55e5f1879cb175203a7f3a
RubenReyes00 <153701176+RubenReyes00@users.noreply.github.com>
2025-04-03 17:30:54 -0600

    cambios temporales
    son lo q llevo por q voy a formatear la lap xd, orita lo continuo por si lo lees nestor

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+
+import React from "react";
+import Login from "./views/login.js";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+};
+
+export default App;
